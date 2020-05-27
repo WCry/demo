aba48de83e385cf49dd09b479784611a584f4e48
zxp <627292959@qq.com>
2020-05-27 23:09:08 +0800

    demo更新

--- spring-cloud/cloud-provider-consumer/consul/consoul-feign-client/src/main/java/com/example/consulfeignclient/ConsulFeignClientApplication.java
+++ spring-cloud/cloud-provider-consumer/consul/consoul-feign-client/src/main/java/com/example/consulfeignclient/ConsulFeignClientApplication.java
@@ -1,5 +1,6 @@
 package com.example.consulfeignclient;
 
+
 import com.example.consulfeignapi.HelloService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
