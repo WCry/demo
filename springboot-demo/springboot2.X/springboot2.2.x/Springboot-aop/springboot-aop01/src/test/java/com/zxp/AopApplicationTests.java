df38db7d44279446333e8e49068ecafe2b9c4ef4
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-06-06 17:09:50 +0800

    demo

--- springboot-demo/springboot2.X/springboot2.2.x/Springboot-aop/springboot-aop01/src/test/java/com/zxp/AopApplicationTests.java
+++ springboot-demo/springboot2.X/springboot2.2.x/Springboot-aop/springboot-aop01/src/test/java/com/zxp/AopApplicationTests.java
@@ -1,5 +1,8 @@
 package com.zxp;
 
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +11,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AopApplicationTests {
-
+    /**
+     * 测试类加载前进行
+     */
+    @BeforeClass
+    public static void beforeTestClass() {
+        System.out.println("测试类之前");
+    }
+    /**
+     * 测试方法加载之前运行，每一个测试类之前都会运行
+     */
+    @Before
+    public void beforeTest() {
+        System.out.println("测试前");
+    }
+    /**
+     * 测试方法
+     */
     @Test
     public void contextLoads() {
+        System.out.println("测试初始化");
     }
-
+    @Test
+    public void Test2() {
+        System.out.println("测试2");
+    }
 }
