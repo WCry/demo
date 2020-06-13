09b384c8be7c68928062710fd50f1d6cf2fa603d
zxp <627292959@qq.com>
2020-06-13 10:46:42 +0800

    demo更新

--- spring-cloud/springcloud-sso/sso-m-master/sso-client2/src/main/java/com/zxp/soo/client/config/SsoClient2Config.java
+++ spring-cloud/springcloud-sso/sso-m-master/sso-client2/src/main/java/com/zxp/soo/client/config/SsoClient2Config.java
@@ -1,19 +1,12 @@
 package com.zxp.soo.client.config;
 
-import com.zxp.sso.mobile.MobileCodeAuthenticationProvider;
-import com.zxp.sso.mobile.UsernamePasswordAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.ServletException;
@@ -36,6 +29,7 @@ public class SsoClient2Config extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
        //应用配置 自定义安全配置  SecurityConfigurerAdapter  配置自定义的Se
        //  http.apply()  通过配置文件处理
       //  http.securityContext(null);
@@ -44,14 +38,14 @@ public class SsoClient2Config extends WebSecurityConfigurerAdapter {
 //                .permitAll();
     }
 
-    public UsernamePasswordAuthenticationProvider usernamePasswordAuthenticationProvider() {
-        return new UsernamePasswordAuthenticationProvider();
-    }
-
-
-    public MobileCodeAuthenticationProvider mobileCodeAuthenticationProvider() {
-        return new MobileCodeAuthenticationProvider();
-    }
+//    public UsernamePasswordAuthenticationProvider usernamePasswordAuthenticationProvider() {
+//        return new UsernamePasswordAuthenticationProvider();
+//    }
+//
+//
+//    public MobileCodeAuthenticationProvider mobileCodeAuthenticationProvider() {
+//        return new MobileCodeAuthenticationProvider();
+//    }
 
     class logoutSuccessHandler implements LogoutSuccessHandler {
 
