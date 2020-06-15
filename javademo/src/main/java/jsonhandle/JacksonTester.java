aad14c0e726ad697bde696d2fbebbf8af32066ba
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-06-15 14:47:50 +0800

    demo

--- javademo/src/main/java/jsonhandle/JacksonTester.java
+++ javademo/src/main/java/jsonhandle/JacksonTester.java
@@ -1,73 +1,114 @@
 package jsonhandle;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.w3c.dom.ls.LSInput;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
- public class JacksonTester {
-        public static void main(String args[]){
-            JacksonTester tester = new JacksonTester();
-            try {
-                ObjectMapper mapper = new ObjectMapper();
-                Map<String,Object> studentDataMap = new HashMap<String,Object>();
-                int[] marks = {1,2,3};
-                Student student = new Student();
-                student.setAge(10);
-                student.setName("Mahesh");
-                // JAVA Object
-                studentDataMap.put("student", student);
-                // JAVA String
-                studentDataMap.put("name", "Mahesh Kumar");
-                // JAVA Boolean
-                studentDataMap.put("verified", Boolean.FALSE);
-                studentDataMap.put("url", new URL("file://D:/"));
-                // Array
-                studentDataMap.put("marks", marks);
-                mapper.writeValue(new File("student.json"), studentDataMap);
-                //result student.json
-                //{
-                //   "student":{"name":"Mahesh","age":10},
-                //   "marks":[1,2,3],
-                //   "verified":false,
-                //   "name":"Mahesh Kumar"
-                //}
+import java.util.Stack;
 
-                System.out.println("class:        "+studentDataMap.get("url").getClass());
-                System.out.println(studentDataMap.get("url"));
-                System.out.println(studentDataMap.get("name"));
-                System.out.println(studentDataMap.get("verified"));
-                System.out.println(studentDataMap.get("marks"));
-            } catch (JsonParseException e) {
-                e.printStackTrace();
-            } catch (JsonMappingException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+public class JacksonTester {
+
+
+    public static void main(String args[]) throws IOException {
+       // TestList();
+        TestMap();
+    }
+
+
+    public static void TestMap() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> studentDataMap = new HashMap<String, Object>();
+            int[] marks = {1, 2, 3};
+            Student student = new Student();
+            student.setAge(10);
+            student.setName("Mahesh");
+            // JAVA Object
+            studentDataMap.put("student", student);
+            // JAVA String
+            studentDataMap.put("name", "Mahesh Kumar");
+            // JAVA Boolean
+            studentDataMap.put("verified", Boolean.FALSE);
+            studentDataMap.put("url", new URL("file://D:/"));
+            // Array
+            studentDataMap.put("marks", marks);
+            mapper.writeValue(new File("student.json"), studentDataMap);
+            //result student.json
+            //{
+            //   "student":{"name":"Mahesh","age":10},
+            //   "marks":[1,2,3],
+            //   "verified":false,
+            //   "name":"Mahesh Kumar"
+            //}
+            JsonNode jsonNode = mapper.readTree(new File("student.json"));
+            System.out.println("class:        " + jsonNode.get("url").getClass());
+            System.out.println(jsonNode.get("url"));
+            System.out.println(jsonNode.get("name"));
+            System.out.println(jsonNode.get("verified"));
+            System.out.println(jsonNode.get("marks"));
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    class Student {
-        private String name;
-        private int age;
-        public Student(){}
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-        public int getAge() {
-            return age;
-        }
-        public void setAge(int age) {
-            this.age = age;
-        }
-        public String toString(){
-            return "jsonhandle.Student [ name: "+name+", age: "+ age+ " ]";
-        }
+
+
+    public static void TestList() throws IOException {
+        List<Student> students = new ArrayList<>();
+        Student student = new Student();
+        student.setAge(10);
+        student.setName("Mahesh");
+        Student student2 = new Student();
+        student2.setAge(10);
+        student2.setName("Mahesh2");
+        students.add(student);
+        students.add(student2);
+        ObjectMapper objectMapper = new ObjectMapper();
+        //转换一个Json的 集合对象
+        List<Student> studentsConvert = objectMapper.readValue(objectMapper.writeValueAsString(students), new TypeReference<List<Student>>() {});
+        System.out.println(studentsConvert.get(0).getName());
     }
+
+}
+
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "jsonhandle.Student [ name: " + name + ", age: " + age + " ]";
+    }
+}
