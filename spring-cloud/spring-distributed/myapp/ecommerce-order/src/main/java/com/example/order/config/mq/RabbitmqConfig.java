cfb94f2de5dfa78726dad77d3225429afce82769
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-06-16 09:27:54 +0800

    demo

--- spring-cloud/spring-distributed/myapp/ecommerce-order/src/main/java/com/example/order/config/mq/RabbitmqConfig.java
+++ spring-cloud/spring-distributed/myapp/ecommerce-order/src/main/java/com/example/order/config/mq/RabbitmqConfig.java
@@ -6,6 +6,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +19,14 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 public class RabbitmqConfig {
+    //发送 设置Converter
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
+    }
+
     @Bean
     public Queue StockQueue() {
         return new Queue(RabbitMqConfig.QUEUE_INFORM_STOCK, true);
--- spring-cloud/spring-distributed/myapp/ecommerce-stock/src/main/java/com/example/stock/config/ActiveConfig.java
+++ spring-cloud/spring-distributed/myapp/ecommerce-stock/src/main/java/com/example/stock/config/ActiveConfig.java
@@ -1,5 +1,9 @@
 package com.example.stock.config;
 
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +16,11 @@ import org.springframework.context.annotation.Configuration;
 public class ActiveConfig {
 
     @Bean
-    public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
     }
 
 }
