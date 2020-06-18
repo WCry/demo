8d029d789640a817b35a6eac9ada56998d02edfc
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-06-18 10:17:33 +0800

    demo

---
+++ javademo/src/main/java/TestMap.java
@@ -0,0 +1,11 @@
+import java.util.LinkedHashMap;
+
+/**
+ * @author zhangxuepei
+ * @since 3.0
+ */
+public class TestMap {
+    public static void main(String[] args) {
+        LinkedHashMap<String,String> linkedHashMap=new LinkedHashMap();
+    }
+}
