da3dfe714a82fae88a995b166ff6fad6c8992a1c
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-07-10 09:39:19 +0800

    demo

--- javademo/src/main/java/socketio/nettyio/ChatClientHandler.java
+++ javademo/src/main/java/socketio/nettyio/ChatClientHandler.java
@@ -8,6 +8,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 //自定义一个客户端业务处理类
+//需要继承InBoundHandle 或者OutBoundHandle
 public class ChatClientHandler extends SimpleChannelInboundHandler<String> {
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, String msg) throws Exception {
