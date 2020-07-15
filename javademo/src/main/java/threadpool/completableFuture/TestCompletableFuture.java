98a52f17e8a347685fff6c9012e9611e605c4d72
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-07-15 19:49:12 +0800

    demo

--- javademo/src/main/java/threadpool/completableFuture/TestCompletableFuture.java
+++ javademo/src/main/java/threadpool/completableFuture/TestCompletableFuture.java
@@ -1,12 +1,9 @@
 package threadpool.completableFuture;
 
-import java.awt.image.ImageProducer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
