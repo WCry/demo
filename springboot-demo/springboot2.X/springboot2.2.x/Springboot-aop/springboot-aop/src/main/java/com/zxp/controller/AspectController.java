b50f3a109e5f0be8399dc484de20ed93db19bf56
zxp <627292959@qq.com>
2020-07-19 11:53:38 +0800

    demo

--- springboot-demo/springboot2.X/springboot2.2.x/Springboot-aop/springboot-aop/src/main/java/com/zxp/controller/AspectController.java
+++ springboot-demo/springboot2.X/springboot2.2.x/Springboot-aop/springboot-aop/src/main/java/com/zxp/controller/AspectController.java
@@ -1,13 +1,14 @@
 package com.zxp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AspectController {
 
-    @RequestMapping(value = "/aspect-test")
+    @GetMapping(value = "/aspect-test")
     @ResponseBody
     public String aspect() {
         System.out.println("执行aspect方法");
