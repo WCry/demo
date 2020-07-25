ece07fdd9f03c94afdf8d3ffa2882851d1f7bd53
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-07-25 15:28:43 +0800

    demo

--- commondemo/kafka/src/main/java/com/example/kafkatest/demo/DemoApplication.java
+++ commondemo/kafka/src/main/java/com/example/kafkatest/demo/DemoApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * kafka消费默认从当前已经存在偏移位置消费
  * 默认不会消费Consumer上线前的消息，
  * 需要主动设置offset进行重新消费
+ * Kafka的assign和subscribe订阅模式和手动提交偏移量
+ * subscribe:group起作用
+ * assign:手动绑定消费分区方式，Group设置不起作用
  */
 @SpringBootApplication
 public class DemoApplication {
