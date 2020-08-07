22417f0e53873ed5fd5a4fcc72c5131a27988246
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-08-07 19:10:56 +0800

    demo

---
+++ javademo/src/main/java/sec/MD5Util.java
@@ -0,0 +1,23 @@
+package sec;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+/**
+ * @author zhangxuepei
+ * @since 3.0
+ */
+public class MD5Util {
+    public static void main(String[] args) {
+       String plainText= "dassssssssssssssssdddddddddddddddddddddddssssss1";
+        byte[] secretBytes = null;
+        try {
+            secretBytes = MessageDigest.getInstance("md5").digest(plainText.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("没有这个md5算法！");
+        }
+        String md5Info=Base64.getEncoder().encodeToString(secretBytes);
+        System.out.println(md5Info);
+    }
+}
