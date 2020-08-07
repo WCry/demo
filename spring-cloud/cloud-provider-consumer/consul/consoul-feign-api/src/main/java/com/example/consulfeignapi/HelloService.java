3e4078eb5aa9b1219c626e752c3699cbf5557116
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-08-07 18:58:30 +0800

    demo

--- spring-cloud/cloud-provider-consumer/consul/consoul-feign-api/src/main/java/com/example/consulfeignapi/HelloService.java
+++ spring-cloud/cloud-provider-consumer/consul/consoul-feign-api/src/main/java/com/example/consulfeignapi/HelloService.java
@@ -2,6 +2,11 @@ package com.example.consulfeignapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+
+import java.util.Map;
 
 
 public interface HelloService {
@@ -14,4 +19,7 @@ public interface HelloService {
     String hello22();
     @PostMapping("/gender")
     Gender getGender(String name);
+
+    @PostMapping("/testRequestParams")
+    Gender testBodyAndQueryParams(@RequestBody byte[] bytes, @RequestParam Map<String, Object> requestParams);
 }
--- spring-cloud/cloud-provider-consumer/consul/consoul-feign-client/src/main/java/com/example/consulfeignclient/HelloImpl.java
+++ spring-cloud/cloud-provider-consumer/consul/consoul-feign-client/src/main/java/com/example/consulfeignclient/HelloImpl.java
@@ -5,6 +5,8 @@ import com.example.consulfeignapi.HelloService;
 import com.example.consulfeignapi.Language;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 /**
  * @author zhangxuepei
  * @since 3.0
@@ -31,4 +33,17 @@ public class HelloImpl implements HelloService {
     public Gender getGender(String name) {
         return Gender.MAN;
     }
+
+    @Override
+    public Gender testBodyAndQueryParams(byte[] bytes, Map<String, Object> requestParams) {
+        System.out.println(bytes.length);
+        System.out.println(requestParams.get("test"));
+        return null;
+    }
+
+//    @Override
+//    public Gender testBodyAndQueryParams(String name, Map<String, Object> requestParams) {
+//        System.out.println(requestParams.get("test"));
+//        return null;
+//    }
 }
