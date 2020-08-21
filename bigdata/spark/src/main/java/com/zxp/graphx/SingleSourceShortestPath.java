d616711d1e94768684c8fd2a1f3c2ed8d5069d79
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-08-21 20:39:40 +0800

    demo

--- bigdata/spark/src/main/java/com/zxp/graphx/SingleSourceShortestPath.java
+++ bigdata/spark/src/main/java/com/zxp/graphx/SingleSourceShortestPath.java
@@ -1,8 +1,7 @@
 package com.zxp.graphx;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -12,14 +11,9 @@ import org.apache.spark.graphx.EdgeTriplet;
 import org.apache.spark.graphx.Graph;
 import org.apache.spark.graphx.Pregel;
 import org.apache.spark.graphx.util.GraphGenerators;
-import scala.Function1;
-import scala.Function2;
+//报错 不影响使用
+import scala.Predef.$eq$colon$eq;
 import scala.Tuple2;
-//主要是Scala的版本冲突  稍后解决
-import scala.Predef.*;//
-// .$eq$colon$eq$;
-
-
 import scala.collection.Iterator;
 import scala.collection.JavaConverters;
 import scala.reflect.ClassTag;
@@ -28,78 +22,75 @@ import scala.runtime.AbstractFunction1;
 import scala.runtime.AbstractFunction2;
 import scala.runtime.AbstractFunction3;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author zhangxuepei
  * @since 3.0
  */
 public class SingleSourceShortestPath {
 
-    static abstract class SerializableFunction1<T1, R> extends AbstractFunction1<T1, R>
-            implements Serializable{
-
-    }
-
-    static abstract class SerializableFunction2<T1, T2, R> extends AbstractFunction2<T1, T2, R>
-            implements Serializable{
-
-    }
-
-    static abstract class SerializableFunction3<T1, T2, T3, R> extends AbstractFunction3<T1, T2, T3, R>
-            implements Serializable{
-
-    }
     private static final ClassTag<Double> tagDouble = ClassTag$.MODULE$.apply(Double.class);
-
-    private static final $eq$colon$eq eqDouble = new $eq$colon$eq(){
+    private static final $eq$colon$eq eqDouble = new $eq$colon$eq() {
         @Override
         public Double apply(Object arg0) {
             return Double.parseDouble(arg0.toString());
-        };
-    };
+        }
 
+        ;
+    };
+    //需要在环境变量中配置HADOOP_HOME路径
+    private static String appName = "spark.demo";
+    //调试环境必须设置成为local[*]， 提交到集群上在设置集群master
+    private static String master = "local[*]";
 
     public static void main(String[] args) {
 
-        SparkContext sc = new JavaSparkContext(new SparkConf().setMaster("local").setAppName("EdgeCount")).sc();
-        Graph<Object, Double> shortGraph =
-                GraphGenerators.logNormalGraph(sc, 100, 1, 4.0, 1.3, -1).mapEdges(new SerializableFunction1<Edge<Object>, Double>(){
+        //修改日志级别
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        //初始化 JavaSparkContext
+        SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+        // conf.setSparkHome("D:\\hadoop-2.9.2\\bin");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        //产生一百个随机顶点的图
+        Graph<Object, Double> shortGraph = GraphGenerators.logNormalGraph(sc, 100, 1, 4.0, 1.3, -1).
+                mapEdges(new SerializableFunction1<Edge<Object>, Double>() {
+                    @Override
+                    public Double apply(Edge<Object> a) {
+                        return Double.parseDouble(a.attr().toString());
+                    }
 
-            @Override
-            public Double apply(Edge<Object> a) {
-
-                return Double.parseDouble(a.attr().toString());
-            }
-
-        },tagDouble);
+                }, tagDouble);
+        //分析起点
         Integer verteId = 42;
-
-        //通过操作顶点映射成为一个新的图
+        //通过操作顶点映射成为一个新的图 初始化顶点  起点设置成为0
         Graph<Double, Double> initalGraph = shortGraph.mapVertices(new SerializableFunction2<Object, Object, Double>() {
             @Override
             public Double apply(Object a, Object b) {
-                if(Double.parseDouble(a.toString()) == verteId)
+                if (Double.parseDouble(a.toString()) == verteId)
                     b = 0.0;
                 else
                     b = Double.POSITIVE_INFINITY;
-                return (Double)b;
+                return (Double) b;
             }
 
-        }, tagDouble,eqDouble);
+        }, tagDouble, eqDouble);
 
         Pregel.apply(initalGraph, Double.POSITIVE_INFINITY, Integer.MAX_VALUE, EdgeDirection.Out(),
-                //  scala.Function3<Object,VD,A,VD> vprog
-                new SerializableFunction3<Object, Double, Double,Double>() {
+                new SerializableFunction3<Object, Double, Double, Double>() {
 
                     @Override
                     public Double apply(Object a, Double b, Double c) {
-                        System.out.println("b before"+ b);
+                        System.out.println("b before" + b);
+                        //获取BC中最小值
                         b = Math.min(b, c);
                         System.out.println("b after" + b);
                         return b;
                     }
                 },
-                // scala.Function1<EdgeTriplet<VD,ED>,scala.collection.Iterator<scala.Tuple2<Object,A>>> sendMsg
-                new SerializableFunction1<EdgeTriplet<Double,Double> ,scala.collection.Iterator<scala.Tuple2<Object, Double>>>(){
+                new SerializableFunction1<EdgeTriplet<Double, Double>, scala.collection.Iterator<scala.Tuple2<Object, Double>>>() {
 
                     /**
                      *  public Iterator<Tuple2<Object, Integer>> apply(EdgeTriplet<Integer, Boolean> et) {
@@ -114,9 +105,9 @@ public class SingleSourceShortestPath {
                     @Override
                     public Iterator<Tuple2<Object, Double>> apply(EdgeTriplet<Double, Double> a) {
                         // TODO Auto-generated method stub
-                        System.out.println("SrcId"+a.srcId()+"SrcAttr"+a.srcAttr()+"EdgeAttr"+a.attr()+"dstId"+a.dstId()+"dstAttr"+a.dstAttr() );
-                        if(a.srcAttr() + a.attr() < a.dstAttr()){
-                            return  JavaConverters.asScalaIteratorConverter(Arrays.asList(new Tuple2<Object, Double>(a.srcId(),a.srcAttr() + a.attr())).iterator()).asScala();
+                        System.out.println("SrcId" + a.srcId() + "SrcAttr" + a.srcAttr() + "EdgeAttr" + a.attr() + "dstId" + a.dstId() + "dstAttr" + a.dstAttr());
+                        if (a.srcAttr() + a.attr() < a.dstAttr()) {
+                            return JavaConverters.asScalaIteratorConverter(Arrays.asList(new Tuple2<Object, Double>(a.srcId(), a.srcAttr() + a.attr())).iterator()).asScala();
                         }
                         return JavaConverters.asScalaIteratorConverter(new ArrayList<Tuple2<Object, Double>>().iterator()).asScala();
                     }
@@ -128,9 +119,21 @@ public class SingleSourceShortestPath {
 
                         return Math.min(a, b);
                     }
-                }, tagDouble, tagDouble,tagDouble);
+                }, tagDouble, tagDouble, tagDouble);
         //.vertices().toJavaRDD().foreach(f->System.out.println("Show the generated graph"+f));
         sc.stop();
+    }
+
+    static abstract class SerializableFunction1<T1, R> extends AbstractFunction1<T1, R> implements Serializable {
+
+    }
+
+    static abstract class SerializableFunction2<T1, T2, R> extends AbstractFunction2<T1, T2, R> implements Serializable {
+
+    }
+
+    static abstract class SerializableFunction3<T1, T2, T3, R> extends AbstractFunction3<T1, T2, T3, R> implements Serializable {
+
     }
 }
 
