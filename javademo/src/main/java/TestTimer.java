f264de8095eadef403a5cac052ecc1ac029dc45c
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-09-09 20:15:11 +0800

    demo

--- javademo/src/main/java/TestTimer.java
+++ javademo/src/main/java/TestTimer.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xerces.internal.impl.xs.traversers.XSAttributeChecker;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -9,6 +11,9 @@ public class TestTimer {
     public static void main(String[] args) {
         Timer timer=new Timer();
         //调度最小时间是1毫秒
+        //最小的线程睡眠时间是1毫秒
+        // Thread.sleep(1);
+
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
