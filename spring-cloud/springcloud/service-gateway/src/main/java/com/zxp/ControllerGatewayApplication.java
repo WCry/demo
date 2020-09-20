f4f33028f78f6ef6e94c32f06314db6be49dc9c5
zxp <627292959@qq.com>
2020-09-20 22:46:25 +0800

    demo

--- spring-cloud/springcloud/service-gateway/src/main/java/com/zxp/ControllerGatewayApplication.java
+++ spring-cloud/springcloud/service-gateway/src/main/java/com/zxp/ControllerGatewayApplication.java
@@ -2,15 +2,15 @@ package com.zxp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 /**
  * 自身应用调用网关
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableOAuth2Sso
+@EnableWebFluxSecurity
 public class ControllerGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ControllerGatewayApplication.class, args);
