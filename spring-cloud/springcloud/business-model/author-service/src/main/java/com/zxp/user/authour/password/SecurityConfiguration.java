8db11dc755e256dc918a03e671d26099e51e775f
zxp <627292959@qq.com>
2020-09-21 21:50:14 +0800

    demo

--- spring-cloud/springcloud/business-model/author-service/src/main/java/com/zxp/user/authour/password/SecurityConfiguration.java
+++ spring-cloud/springcloud/business-model/author-service/src/main/java/com/zxp/user/authour/password/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
@@ -20,7 +21,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         this.loginSuccessHandler = loginSuccessHandler;
         this.jdbcTokenRepository = jdbcTokenRepository;
     }
-
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/actuator/health");
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
         //  auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
@@ -30,12 +34,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().loginPage("/authentication/require").permitAll().
-                successHandler(loginSuccessHandler).and().authorizeRequests()
-                .antMatchers("/images/**", "/checkCode", "/scripts/**", "/styles/**")
-                .permitAll().anyRequest().authenticated()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
-                .and().exceptionHandling().accessDeniedPage("/deny")
-                .and().rememberMe().tokenValiditySeconds(86400).tokenRepository(jdbcTokenRepository);
+        http.formLogin().loginPage("/authentication/require").
+                successHandler(loginSuccessHandler);
+//                .and().authorizeRequests()
+//                .antMatchers("/images/**", "/checkCode", "/scripts/**", "/styles/**")
+//                .permitAll().anyRequest().authenticated();
+//                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+//                .and().exceptionHandling().accessDeniedPage("/deny")
+//                .and().rememberMe().tokenValiditySeconds(86400).tokenRepository(jdbcTokenRepository);
     }
 }
