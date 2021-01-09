59fc92098abec613c2fd22243471d6bf5d98e73f
zxp <627292959@qq.com>
2021-01-09 21:20:20 +0800

    demo

--- springboot-demo/springboot2.X/springboot2.2.x/springboot-monitor/springboot-actuator-prometheus/src/main/java/com/zxp/SpringbootActuatorPrometheusDemoApplication.java
+++ springboot-demo/springboot2.X/springboot2.2.x/springboot-monitor/springboot-actuator-prometheus/src/main/java/com/zxp/SpringbootActuatorPrometheusDemoApplication.java
@@ -1,17 +1,14 @@
-package com.gf;
+package com.zxp;
 
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @RestController
-public class SpringbootActuatorPrometheusDemoApplication {
+public class SpringbootActuatorPrometheusDemoApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootActuatorPrometheusDemoApplication.class, args);
--- springboot-demo/springboot2.X/springboot2.2.x/springboot-monitor/springboot-actuator-prometheus/src/test/java/com/zxp/SpringbootActuatorPrometheusDemoApplicationTests.java
+++ springboot-demo/springboot2.X/springboot2.2.x/springboot-monitor/springboot-actuator-prometheus/src/test/java/com/zxp/SpringbootActuatorPrometheusDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gf;
+package com.zxp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
